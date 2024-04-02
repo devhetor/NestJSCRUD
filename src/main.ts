67ea2e2f85ffce35c1d98e7f4036cf18c369ffc1
Thiago Arcoverde <tluiz2701@gmail.com>
2024-04-02 09:51:58 -0300

    adicionado swagger para documentação

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { logger } from './common/logger.middleware';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Tópicos em programação')
+    .setDescription('CRUD usando NestJS')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.use(logger)
   await app.listen(3000);
 }
